4b3a87e87897b1170502dbe94b8e0ae4dc839ba7
anwarfaisal320 <anwar.faisal@gmail.com>
2022-12-12 11:56:47 -0500

    initial gke terraform changes

---
+++ provider.tf
@@ -0,0 +1,25 @@
+# Google Cloud Platform Provider
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project = "single-ripsaw-364416"
+  region = "us-central1"
+}
+
+provider "google-beta" {
+#version = "~> 3.66.0"
+ project = "single-ripsaw-364416"
+ region = "us-central1"
+}
+terraform {
+  backend "gcs" {
+    bucket = "anwar-tf-state-staging"
+    prefix = "terraform/state"
+  }
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~> 3.66"
+#     }
+#   }
+  
+}
